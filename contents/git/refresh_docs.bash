68df75835306ddc1e9ab295dd0d9d4bd8ef28ef4
Han Jiang <jhcarl0814@gmail.com>
2026-05-30 15:45:29 +1200

    git 2.54 reference desk; change from ansi2html to mintty_html_dump

--- contents/git/refresh_docs.bash
+++ contents/git/refresh_docs.bash
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-export PATH="$PATH:$(realpath '../../tools/awk_expand_commands'):$(realpath '../../tools/tree'):$(realpath '../../tools/ansi2html')";
-
-function ansi2html_pre () {
-	printf '<pre class="f9 b9">';
-	ansi2html --bg=dark --palette=xterm --body-only;
-	printf '</pre>';
-}
-export -f ansi2html_pre;
+export PATH="$PATH:$(realpath '../../tools/awk_expand_commands'):$(realpath '../../tools/tree'):$(realpath '../../tools/awk_remove_heredoc_leading_spaces'):$(realpath '../../tools/mintty_html_dump')";
 
 function ls() { command ls --color=always "$@"; }
 export -f ls;
 
-mkdir --parents '../../docs/git'
+export GIT_PAGER='cat';
+
+mkdir --parents -- '../../docs/git';
+
+rm --force --recursive -- '/test_git_mintty_html_dump_temp_dir'; mkdir --parents -- "$_"; mintty_html_dump_temp_dir="$_";
+export mintty_html_dump_temp_dir;
 
 # awk_expand_commands './git.template.html' | tee '../../docs/git/git.html' | awk '{ printf("| %s%s", $0, RT); }; END { if (RT == "" && $0 == "") printf("| \n"); else { if (RT == "") printf("\n"); else printf("| \n"); } }';
 awk_expand_commands './git.template.html' >'../../docs/git/git.html'
--- contents/git/refresh_docs_when_contents_change.bash
+++ contents/git/refresh_docs_when_contents_change.bash
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-export PATH="$PATH:$(realpath '../../tools/awk_expand_commands'):$(realpath '../../tools/tree'):$(realpath '../../tools/ansi2html')";
-
-function ansi2html_pre () {
-	printf '<pre class="f9 b9">';
-	ansi2html --bg=dark --palette=xterm --body-only;
-	printf '</pre>';
-}
-export -f ansi2html_pre;
+export PATH="$PATH:$(realpath '../../tools/awk_expand_commands'):$(realpath '../../tools/tree'):$(realpath '../../tools/awk_remove_heredoc_leading_spaces'):$(realpath '../../tools/mintty_html_dump')";
 
 function ls() { command ls --color=always "$@"; }
 export -f ls;
 
-mkdir --parents '../../docs/git'
+export GIT_PAGER='cat';
+
+mkdir --parents -- '../../docs/git';
+
+rm --force --recursive -- '/test_git_mintty_html_dump_temp_dir'; mkdir --parents -- "$_"; mintty_html_dump_temp_dir="$_";
+export mintty_html_dump_temp_dir;
 
 time_of_last_access_last=;
 
@@ -22,10 +20,13 @@ do
 
 	if [[ "$time_of_last_access" != "$time_of_last_access_last" ]]
 	then
-		printf '%s (%s)\n' "$(date +'%Y-%m-%d %H:%M:%S%:z')" "$(date --utc +'%Y-%m-%d %H:%M:%S%:z')";
+		printf 'refresh_begin: %s (%s)\n' "$(date +'%Y-%m-%d %H:%M:%S%:z')" "$(date --utc +'%Y-%m-%d %H:%M:%S%:z')";
 
 		# awk_expand_commands './git.template.html' | tee '../../docs/git/git.html' | awk '{ printf("| %s%s", $0, RT); }; END { if (RT == "" && $0 == "") printf("| \n"); else { if (RT == "") printf("\n"); else printf("| \n"); } }';
 		awk_expand_commands './git.template.html' >'../../docs/git/git.html'
+		printf '\n';
+
+		printf 'refresh_end: %s (%s)\n' "$(date +'%Y-%m-%d %H:%M:%S%:z')" "$(date --utc +'%Y-%m-%d %H:%M:%S%:z')";
 		printf '\n';
 
 		time_of_last_access_last="$time_of_last_access";
